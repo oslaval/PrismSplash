d081391d7be77bdd575a9943a96f0c65041327c6
54B074G3 <oslavalgooglemail.com>
2020-10-25 21:22:21 +0100

    cleanup

--- Prism.Splash/Views/SplashView.xaml.cs
+++ Prism.Splash/Views/SplashView.xaml.cs
@@ -1,29 +1,17 @@
-﻿#region File Info Header
-/*________________________________________________________________________________________
-
-  Copyright (C) 2011 Jason Zhang, eagleboost@msn.com
-
-  * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-  * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
-  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-
-________________________________________________________________________________________*/
-#endregion File Info Header
+﻿using Splash.ViewModels;
 
 namespace Splash.Views
 {
-  using ViewModels;
-
-  /// <summary>
-  /// SplashView.xaml 的交互逻辑
-  /// </summary>
-  public partial class SplashView
-  {
-    public SplashView(SplashViewModel viewModel)
+    /// <summary>
+    /// SplashView.xaml
+    /// </summary>
+    public partial class SplashView
     {
-      InitializeComponent();
+        public SplashView(SplashViewModel viewModel)
+        {
+            InitializeComponent();
 
-      DataContext = viewModel;
+            DataContext = viewModel;
+        }
     }
-  }
 }
