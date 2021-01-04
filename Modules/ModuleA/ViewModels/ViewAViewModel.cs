723edf54d27d829d6d0d59f3ef993d999e25d7e1
54B074G3 <oslavalgooglemail.com>
2021-01-04 23:34:39 +0100

    code cleanup

--- Modules/ModuleA/ViewModels/ViewAViewModel.cs
+++ Modules/ModuleA/ViewModels/ViewAViewModel.cs
@@ -1,11 +1,4 @@
-﻿using Prism.Commands;
-using Prism.Events;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Prism.Mvvm;
 
 namespace ModuleA.ViewModels
 {
